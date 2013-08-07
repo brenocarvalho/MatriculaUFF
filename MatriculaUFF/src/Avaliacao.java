c10b8f966a2909123a2ac3f213fb7c6362c21592
Breno Carvalho <brenowcarvalho@gmail.com>
2013-08-07 19:36:02 -0300

    Fixing some merging mistakes

---
+++ MatriculaUFF/src/Avaliacao.java
@@ -0,0 +1,20 @@
+
+/**
+ *
+ * @author Breno W. Carvalho, C. Bruno Garcia, Ian Villar, Rafael Grillo
+ */
+public class Avaliacao
+{
+
+    private int nota;
+
+    public int getNota()
+    {
+        return nota;
+    }
+
+    public void setNota(int nota)
+    {
+        this.nota = nota;
+    }
+}
